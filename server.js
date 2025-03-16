0c9f1d1f15ea8d83d19807ced70c73684e2fe835
ovinvinwath <128613702+ovinvinwath@users.noreply.github.com>
2025-03-16 19:40:56 +1100

    uploaded files

---
+++ server.js
@@ -0,0 +1,28 @@
+//Detailed doccumentation on how to do this task -->
+// created the directory sit323-2025-prac2p
+// initialize project using npm init on terminal
+// install express using npm install express --save on terminal
+// copy below code onto a new Server.js file
+// create a simple HTML page as i have seperately
+// launch server by typing 'node .' or 'node server.js' on terminal
+
+// trouble shooting tips-->
+// you may need to adjust the port number if loading errors occur
+// you may need to use the 'const path' code line to resolve any path issues that may occur when starting Server.
+
+const express = require('express');
+const path = require('path'); //needed to resolve path file correctly 
+
+const app = express();
+const port = 5000;
+
+
+// serving HTML file on server
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage.html'));
+});
+
+//Start server
+app.listen(port, () => {
+    console.log(`This app is listening at http://localhost:${port}`);
+});
